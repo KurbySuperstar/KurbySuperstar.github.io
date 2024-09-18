41e37b78317fe57644f698750fea9ece0226f581
Herperger5802317 <5802317@spsd.sk.ca>
2024-09-18 12:16:43 -0600

    update 2 to sketch

--- 04 interactive scene/sketch.js
+++ 04 interactive scene/sketch.js
@@ -8,20 +8,27 @@
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-}
-
+  stroke (255,255,255)
+  line (windowWidth,windowHeight,0,0)
+  textSize(16);
+  textAlign(CENTER,BOTTOM);
+  text('ABCD', 50, 30);
+} 
+var currentBack = 0
+var move = 0
 function draw() {
-  background(0);
+  bG();
+  //background(0,0,0)
   noStroke()
   fill(255,255,255);
   ellipse(windowWidth/2,windowHeight,windowHeight*2,windowHeight*0.15);
   if (mouseX > windowWidth*0.5){
     fill(255,0,0);
-    circle(windowHeight*0.2,windowWidth*0.1,200);
+    circle(windowHeight*0.2 +move,windowWidth*0.1,200);
   }
   else{
     fill(255,255,0);
-  circle(windowHeight*0.2,windowWidth*0.1,200);
+  circle(windowHeight*0.2 + move  ,windowWidth*0.1,200);
 
   }
   fill(0,0,0);
@@ -33,4 +40,46 @@ function draw() {
   line(mouseX,mouseY-10,mouseX-25,mouseY+15);
   line(mouseX,mouseY+50,mouseX+25,mouseY+75);
   line(mouseX,mouseY+50,mouseX-25,mouseY+75);
+  moveSun()
+}
+
+function bG(){
+  if (currentBack === 0){
+    background(0);
+  }
+  else if (currentBack === 1){
+    background(255,0,0);
+  }
+  else if (currentBack ===2){
+    background(0,255,0);
+  }
+  else{
+    background(0,0,255);
+  }
+
+}
+function mousePressed(){
+  if (mouseButton === CENTER && currentBack ===0){
+    currentBack+=1
+  }
+  else if (mouseButton === CENTER && currentBack === 1){
+    currentBack += 1
+  }
+  else if (mouseButton === CENTER && currentBack === 2){
+    currentBack+=1
+  }
+  else if (mouseButton === CENTER&& currentBack === 3){
+    currentBack-=3
+  }
+}
+function moveSun(){
+  if (move > windowWidth){
+    move -= windowWidth 
+  }
+  else if (keyIsPressed === true){
+    move += 10    
+  }
+
+}
+function text(){
 }
