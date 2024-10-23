f5bbb9c94e3cd9827bcc577b80614acf295a5535
Herperger5802317 <5802317@spsd.sk.ca>
2024-10-23 12:12:49 -0600

    CARSCARSCARSCRSCRASRCRSCRRCSRCSCRSASCRSACRSCRSCARCSRCARACSRACSRCSRCSACSARCSARCSRCSRCSRCASR

--- 19 CARS CARS CARS CARS CARS CARS CARS CARS CARS CARS CARS CARS CARS CARS ARS CARS CARS CARS CARS CARS CARS CARS CARS CARS CARS CARS CARS CARS CARS CARS CARS/sketch.js
+++ 19 CARS CARS CARS CARS CARS CARS CARS CARS CARS CARS CARS CARS CARS CARS ARS CARS CARS CARS CARS CARS CARS CARS CARS CARS CARS CARS CARS CARS CARS CARS CARS/sketch.js
@@ -7,9 +7,13 @@ let dash2 = 20;
 let myCar;
 let eastbound = [];
 let westbound = [];
+let light;
+let count;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  count = 0;
+  frameRate(60);
 }
 
 function draw() {
@@ -28,7 +32,7 @@ function drawRoad(){
   fill(0);
   noStroke();
   rect(0,height/3.5,width,400);
-  stroke(255);
+  stroke(255,225,0);
   strokeWeight(5);
   while (dash1 < width){
     line(dash1,height/2,dash2,height/2);
@@ -64,11 +68,13 @@ class vehicle{
   }
   action(){
     //call the other functions
+    this.trafficLight();
     this.speedup();
     this.slowdown();
     this.move();
     this.colorchange();
     this.display();
+
   }
   display(){
     if(this.type ===0){
@@ -84,28 +90,37 @@ class vehicle{
     }
   }
   drawCar(){
+    strokeWeight(1);
+    stroke(255);
     fill(this.colour);
     rect(this.x,this.y,100,40);
 
   }
   drawTruck(){
+    strokeWeight(1);
+    stroke(255);
     fill(this.colour);
     ellipse(this.x,this.y,100,40);
 
   }
   move(){
     if (this.direction === 1){
-      this.x += this.xspeed;
-      if (this.x > width){
-        this.x =0;
+      if (light === 1){
+
+        this.x += this.xspeed;
+        if (this.x > width){
+          this.x =0;
+        }
       }
   
 
     }
     else if (this.direction === 0){
-      this.x -= this.xspeed;
-      if (this.x < 0){
-        this.x =width;
+      if(light === 1){
+        this.x -= this.xspeed;
+        if (this.x < 0){
+          this.x =width;
+        }
       }
   
 
@@ -132,7 +147,7 @@ class vehicle{
       }
     }
     if (this.direction === 0){
-      if (random(1,100) >= 100){
+      if (random(1,100) >= 100){ 
         xspeed -= random(-15,-1);
       }
     }
@@ -142,5 +157,25 @@ class vehicle{
       this.colour = color(random(255),random(255),random(255));
 
     }
+  }
+  trafficLight(){
+    noStroke();
+    fill("green");
+    light = 1;
+    if(light === 1){
+      rect(width/2, 50,30); 
+    }
+    // if (keyIsPressed &&  keyCode === 32){
+
+    //   light = 0;
+
+
+    //   for (let l = 0; l<240000; l++){
+    //     fill(255,0,0);
+    //     rect(width/2, 50,30);
+
+    //   }
+    // }
+
   }
 }
