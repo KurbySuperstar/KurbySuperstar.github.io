b6cc75eba9d14a6a6adebaa0d785d0e570262987
Herperger5802317 <5802317@spsd.sk.ca>
2024-10-30 12:16:15 -0600

    GAME IS WORK

--- 20 2d array/sketch.js
+++ 20 2d array/sketch.js
@@ -2,26 +2,28 @@
 // Ian Herperger
 // 10/29/2024
 
-const NUM_ROWS = 4;
+const NUM_ROWS = 5;
 const NUM_COLS = 5;
 let winCon = 0;
 let rectWidth, rectHeight;
 let currentRow, currentCol;
-let gridData = [[0,0,0,255,0],
-                [255,0,255,0,255],
-                [0,255,0,0,0],
-                [0,0,255,0,255]];
+let grid =[];
+//let gridData = [[0,0,0,255,0],
+                // [255,0,255,0,255],
+                // [0,255,0,0,0],
+                // [0,0,255,0,255]];
 function setup() {
   createCanvas(windowWidth, windowHeight);
   rectWidth = width / NUM_COLS;
   rectHeight = height / NUM_ROWS;
+  randomize();
 
 }
 function drawGrid(){
 //draw the squares for the grid that appears
   for (let y = 0; y < NUM_ROWS; y++){
     for (let x = 0; x < NUM_COLS; x++){
-      fill(gridData[y][x]);
+      fill(grid[y][x]);
       rect(x*rectWidth,y*rectHeight,rectWidth,rectHeight);
     }
   }
@@ -36,11 +38,11 @@ function determineActiveSquare(){
 
 function flip(col, row){
   if (col >= 0 && col < NUM_COLS && row >= 0 && row < NUM_ROWS){
-    if (gridData[row][col] === 0){
-      gridData[row][col] = 255;
+    if (grid[row][col] === 0){
+      grid[row][col] = 255;
     }
     else{ 
-      gridData[row][col] = 0;
+      grid[row][col] = 0;
     }
   }
 }
@@ -48,8 +50,11 @@ function flip(col, row){
 function draw() {
   background(220);
   determineActiveSquare();
+
   drawGrid();
+
   win();
+  overlay();
 }
 function mousePressed(){
   if(keyIsDown(SHIFT)){
@@ -67,8 +72,8 @@ function mousePressed(){
 
 }
 function win(){
-  let fv = gridData[0][0];
-  let white = gridData.every(row => row.every(value=> value === fv));
+  let fv = grid[0][0];
+  let white = grid.every(row => row.every(value=> value === fv));
   if(white){
     stroke(0);
 
@@ -78,7 +83,7 @@ function win(){
     text("YOU WIN",width/2,height/2);
 
   }
-  let black = gridData.every(row => row.every(value=> value === fv));
+  let black = grid.every(row => row.every(value=> value === fv));
   if(black){
     fill(255);
     textAlign(CENTER);
@@ -87,4 +92,35 @@ function win(){
 
   }
 
+}
+function randomize(){
+  for(let i =0; i< NUM_ROWS;i++){
+    let rowArray = [];
+    for(let i =0; i< NUM_COLS;i++){
+      r = int(random(0,2));
+      if (r === 0){
+        rowArray.push(0);
+      }
+      else{
+        rowArray.push(255);
+      }
+
+    }
+    grid.push(rowArray);
+
+  }
+}
+
+function overlay(){
+  //know which squares are currently no solved
+  currentRow = int(mouseY / rectHeight);
+  currentCol = int(mouseX / rectWidth);
+  print(currentCol, currentRow);
+
+  fill(0,200,0);
+  rect(currentCol*rectWidth,currentRow*rectHeight,rectWidth,rectHeight);
+  rect((currentCol-1)*rectWidth,currentRow*rectHeight,rectWidth,rectHeight);
+  rect((currentCol+1)*rectWidth,currentRow*rectHeight,rectWidth,rectHeight);
+  rect(currentCol*rectWidth,(currentRow-1)*rectHeight,rectWidth,rectHeight);
+  rect(currentCol*rectWidth,(currentRow+1)*rectHeight,rectWidth,rectHeight);
 }
